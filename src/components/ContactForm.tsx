18e058334923a93854134ec91b18b1ba2aee1236
alvarorubio-dev <rubiolopezalvaro@gmail.com>
2026-06-03 12:37:23 -0500

    modificar espacio placeholder-form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -150,7 +150,7 @@ export default function ContactForm() {
                       value={form.phone}
                       onChange={handleChange}
                       required
-                      placeholder="Email oTeléfono"
+                      placeholder="Email o Teléfono"
                       className={inputClass}
                     />
 
